c72f9082fd4cf8ade1211457867a73d3474e27c6
Mike Wozniewski <mike@mikewoz.com>
2009-06-18 16:59:16 +0000

    SVN Revision: 3

--- scripts/posturesConf.sh
+++ scripts/posturesConf.sh
@@ -6,10 +6,14 @@ SPINWIDGETS=~/src/spinwidgets
 SPIN=~/src/spinframework
 
 WHOAMI=`whoami`
+SPINID=`hostname -s`
+
+alias spinViewer='${SPIN}/src/vess/asViewer -viewerID ${SPINID}'
 
 # this can be removed once the code is more stable:
 export LD_LIBRARY_PATH=${SPIN}/src/osgWrappers
 
+# this is for 
 export MY_IP=`perl -I${POSTURES}/scripts -MSys::IP -e "print ip()"`
 case "$TERM" in xterm*|rxvt*)
 	PROMPT_COMMAND='echo -ne "\033]0; ${MY_IP} : ${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007"'
