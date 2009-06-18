98bec11cb6a3e54b3036a53e34e748f57499ed0b
Mike Wozniewski <mike@mikewoz.com>
2009-06-18 18:03:38 +0000

    fixed pdsettings
    SVN Revision: 5

--- scripts/posturesConf.sh
+++ scripts/posturesConf.sh
@@ -7,7 +7,10 @@ SPIN=~/src/spinframework
 
 SPINID=`hostname -s`
 
+`cp ${POSTURES}/scripts/pdsettings ~/.pdsettings`
+
 alias cdvess='cd ${SPIN}/src/vess'
+alias spinEdit='pd SPINedit.pd'
 alias spinViewer='${SPIN}/src/vess/asViewer -viewerID ${SPINID}'
 
 # this can be removed once the code is more stable:
