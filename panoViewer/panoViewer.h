601845d6f63ee10e71ed7cbff4490fe262225f94
Mike Wozniewski <mike@mikewoz.com>
2009-07-09 14:20:53 +0000

    Added panoViewer
    SVN Revision: 17

---
+++ panoViewer/panoViewer.h
@@ -0,0 +1,34 @@
+/*
+ *  panoViewer.h
+ *  Panoscope
+ *
+ *  Created by Gideon May on 12-10-07.
+ *  Copyright 2007 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef PANOVIEWER
+#define PANOVIEWER 1
+
+#include <osg/ArgumentParser>
+#include <osgGA/EventVisitor>
+#include <osgUtil/UpdateVisitor>
+
+#include <osgViewer/GraphicsWindow>
+#include <osgViewer/View>
+
+
+class panoViewer : public osgViewer::Viewer
+{
+public:
+    panoViewer();
+    panoViewer(osg::ArgumentParser& arguments);
+    void setupViewForPanoscope();
+    
+    virtual void requestRedraw();
+    virtual void requestContinuousUpdate(bool needed=true);
+    virtual void requestWarpPointer(float x,float y);
+};
+
+
+#endif
