36be70411d224768bca2f5b7e62176ae14f58a77
Mike Wozniewski <mike@mikewoz.com>
2010-01-13 02:29:26 +0000

    SVN Revision: 140

--- scripts/postureVars.py
+++ scripts/postureVars.py
@@ -20,10 +20,8 @@ class postures:
     spinPath = "%s/spinframework/trunk" % (srcPath)
 
     pd = "%s/pd-0.41-4/bin/pd -jack -r 16000 -inchannels 3 -outchannels 3" % (srcPath)
-    ntaClientDebug = "%s %s/ntaClient.pd" % (pd, ntaPath)
-    ntaClient = "%s -send 'init! bang' %s/ntaClient.pd" % (pd, ntaPath)
-    #spinEditorDebug = "%s %s/patches/spinEdit.pd %s/milhouseTest.pd" % (pd, spinwidgetsPath, ntaPath)
-    spinEditor = "%s -send 'init! bang' %s/patches/spinEdit.pd %s/milhouseTest.pd" % (pd, spinwidgetsPath, ntaPath)
+    ntaClient = "%s %s/ntaClient.pd" % (pd, ntaPath)
+    spinEditor = "%s %s/patches/spinEdit.pd %s/milhouseTest.pd" % (pd, spinwidgetsPath, ntaPath)
 
 
 
