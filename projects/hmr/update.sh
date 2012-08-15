f30bb65384ae242632ed92ac37310e2d849e2c6e
Mike Wozniewski <mike@mikewoz.com>
2012-08-15 01:35:25 -0400

    HMR: After David's clean-up and animation path for camera (but not fully functional

--- projects/hmr/update.sh
+++ projects/hmr/update.sh
@@ -2,5 +2,5 @@
 
 # This gets the latest list of names and adds an escape character for commas because Pd will break up messages when it sees commas.
 
-wget -r http://127.0.0.1/musee/donateurs.php
+wget -r --user=admin --password='viewNX2' http://127.0.0.1/musee/donateurs.php
 sed -i 's/,/\\,/g' 127.0.0.1/musee/donateurs.php
