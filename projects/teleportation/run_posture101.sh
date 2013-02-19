fa32162bbc1ffce5db98b983d4c310b91519e1f1
Bruno Roy <bruno.o.roy@gmail.com>
2013-02-19 11:51:36 -0500

    Added some scripts to teleportation project

---
+++ projects/teleportation/run_posture101.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+killall posture-camera
+killall posture-merge
+rm /tmp/cam*
+
+posture-camera -s /tmp/cam0 -c 0 > /dev/null &
+posture-camera -s /tmp/cam1 -c 1 > /dev/null &
+posture-camera -s /tmp/cam2 -c 2 > /dev/null &
+
+posture-merge -v -s /tmp/pcl /tmp/cam0 /tmp/cam1 /tmp/cam2 > /dev/null &
+sleep 10
+
+./scripts/pcl-posture101-sender.sh
