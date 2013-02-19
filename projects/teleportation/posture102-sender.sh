b24041dcc7fa9b36f153a66b6d50a6a7f4aa44e5
Emmanuel Durand <emmanueldurand@gmail.com>
2013-02-19 14:06:27 -0500

    More updates

--- projects/teleportation/posture102-sender.sh
+++ projects/teleportation/posture102-sender.sh
@@ -9,7 +9,7 @@ posture-camera -s /tmp/cam0 -c 0 > /dev/null &
 posture-camera -s /tmp/cam1 -c 1 > /dev/null &
 posture-camera -s /tmp/cam2 -c 2 > /dev/null &
 
-posture-merge -v -s /tmp/pcl /tmp/cam0 /tmp/cam1 /tmp/cam2 > /dev/null &
+posture-merge -v -C -s /tmp/pcl /tmp/cam0 /tmp/cam1 /tmp/cam2 > /dev/null &
 sleep 10
 
 killall switcher
