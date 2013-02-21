6c71f13cec3b3d712e06ae6fd5db051b5d49efc5
Emmanuel Durand <emmanueldurand@gmail.com>
2013-02-21 09:30:26 -0500

    Updated scripts for teleportation

--- projects/teleportation/posture102-receiver.sh
+++ projects/teleportation/posture102-receiver.sh
@@ -2,3 +2,10 @@
 
 ./scripts/pcl-posture102-receiver.sh
 ./scripts/pulse-posture102-receiver.sh
+
+rm /tmp/posture*
+ln -s /tmp/pcl /tmp/posture102-readsm
+ln -s /tmp/switcher_pclreceiver_pclstream_custom_0 /tmp/posture101-readsm
+
+sleep 5
+gst-launch shmsrc socket-path=/tmp/switcher_pulsereceiver_pulsestream_custom_0 ! gdpdepay ! decodebin2 ! pulsesink sync=false &
