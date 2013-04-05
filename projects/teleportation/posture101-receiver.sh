26ffd76b9bd1c1c912e68f6b9388e5421bd43920
Emmanuel Durand <emmanueldurand@gmail.com>
2013-04-05 16:45:34 -0400

    teleportation : modified main patch + sender and receiver scripts

--- projects/teleportation/posture101-receiver.sh
+++ projects/teleportation/posture101-receiver.sh
@@ -4,7 +4,7 @@
 ./scripts/pulse-posture101-receiver.sh
 
 rm /tmp/posture*
-ln -s /tmp/pcl /tmp/posture101-readsm
+#ln -s /tmp/pcl /tmp/posture101-readsm
 ln -s /tmp/switcher_pclreceiver_pclstream_custom_0 /tmp/posture102-readsm
 
 sleep 5
