fa30d12511e134c2c3732a4bdaeb2ba24ec0a27b
Emmanuel Durand <emmanueldurand@gmail.com>
2013-05-16 09:26:52 -0400

    Teleportation: uploaded modifications done to posture102 scripts

--- projects/teleportation/posture102-receiver.sh
+++ projects/teleportation/posture102-receiver.sh
@@ -4,7 +4,7 @@
 ./scripts/pulse-posture102-receiver.sh
 
 rm /tmp/posture*
-ln -s /tmp/pcl /tmp/posture102-readsm
+#ln -s /tmp/pcl /tmp/posture102-readsm
 ln -s /tmp/switcher_pclreceiver_pclstream_custom_0 /tmp/posture101-readsm
 
 sleep 5
--- projects/teleportation/posture102-sender.sh
+++ projects/teleportation/posture102-sender.sh
@@ -5,12 +5,13 @@ killall posture-merge
 rm /tmp/cam*
 rm /tmp/pcl
 
-posture-camera -s /tmp/cam0 -c 0 > /dev/null &
-posture-camera -s /tmp/cam1 -c 1 > /dev/null &
-posture-camera -s /tmp/cam2 -c 2 > /dev/null &
+posture-camera -H -s /tmp/cam0 -c 0 &
+posture-camera -H -s /tmp/cam1 -c 1 &
+posture-camera -H -s /tmp/cam2 -c 2 &
 
-posture-merge -v -C -s /tmp/pcl /tmp/cam0 /tmp/cam1 /tmp/cam2 > /dev/null &
-sleep 10
+sleep 5
+posture-merge -C -s /tmp/pcl /tmp/cam0 /tmp/cam1 /tmp/cam2 &
+sleep 5
 
 killall switcher
 rm /tmp/switcher*
