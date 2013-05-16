f1d837d43bcdb3d4d75d6dab5124caab2b1e8734
Emmanuel Durand <emmanueldurand@gmail.com>
2013-05-16 13:39:30 -0400

    Teleportation: WIP

--- projects/teleportation/scripts/posture101-receiver.sh
+++ projects/teleportation/scripts/posture101-receiver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Parameters
+SENDER_URL=`echo http://localhost:8250`
 SDP_PCL_FILE_LOCATION_1="http://posture102.local:8260/sdp?rtpsession=pclsender&destination=pclmixer"
 SDP_PCL_FILE_LOCATION_2="http://Linux-Desktop-2.local:8270/sdp?rtpsession=pclsender&destination=pclmixer"
 SDP_PULSE_FILE_LOCATION_1="http://posture102.local:8260/sdp?rtpsession=pulsesender&destination=pulsemixer"
@@ -8,13 +9,13 @@ SDP_PULSE_FILE_LOCATION_2="http://Linux-Desktop-2.local:8260/sdp?rtpsession=puls
 SDP_GET_QUID=uridecodebin
 
 # Point clouds
-switcher-ctrl -C $SDP_GET_QUID pclstream_1
+switcher-ctrl -S $SENDER_URL -C $SDP_GET_QUID pclstream_1
 sleep 1
-switcher-ctrl -i pclstream_1 to_shmdata $SDP_FILE_LOCATION_1
+switcher-ctrl -S $SENDER_URL -i pclstream_1 to_shmdata $SDP_FILE_LOCATION_1
 sleep 2
-switcher-ctrl -C $SDP_GET_QUID pclstream_2
+switcher-ctrl -S $SENDER_URL -C $SDP_GET_QUID pclstream_2
 sleep 1
-switcher-ctrl -i pclstream_2 to_shmdata $SDP_FILE_LOCATION_2
+switcher-ctrl -S $SENDER_URL -i pclstream_2 to_shmdata $SDP_FILE_LOCATION_2
 sleep 2
 
 rm /tmp/posture*
@@ -22,10 +23,10 @@ ln -s /tmp/switcher_pclreceiver_pclstream_1_custom_0 /tmp/posture102-readsm
 ln -s /tmp/switcher_pclreceiver_pclstream_2_custom_0 /tmp/posture103-readsm
 
 # Pulse
-switcher-ctrl -C $SDP_GET_QUID pulsestream_1
+switcher-ctrl -S $SENDER_URL -C $SDP_GET_QUID pulsestream_1
 sleep 1
-switcher-ctrl -i pulsestream_1 to_shmdata $SDP_PULSE_FILE_LOCATION_1
+switcher-ctrl -S $SENDER_URL -i pulsestream_1 to_shmdata $SDP_PULSE_FILE_LOCATION_1
 sleep 2
-switcher-ctrl -C $SDP_GET_QUID pulsestream_2
+switcher-ctrl -S $SENDER_URL -C $SDP_GET_QUID pulsestream_2
 sleep 1
-switcher-ctrl -i pulsestream_2 to_shmdata $SDP_PULSE_FILE_LOCATION_2
+switcher-ctrl -S $SENDER_URL -i pulsestream_2 to_shmdata $SDP_PULSE_FILE_LOCATION_2
--- projects/teleportation/scripts/posture101-sender.sh
+++ projects/teleportation/scripts/posture101-sender.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
-#SENDER_URL=`echo http://localhost:${SENDER_PORT}`
+SENDER_URL=`echo http://localhost:8250`
 DESTINATION_IP_1=posture102.local
 DESTINATION_IP_2=Linux-Desktop-2.local
 PCL_NAME=/tmp/pcl
 
 # Send point clouds
-echo "------ PCL ------"
-switcher-ctrl -C rtpsession pclsender
+echo '------ PCL ------'
+switcher-ctrl -S $SENDER_URL -C rtpsession pclsender
 sleep 1
-switcher-ctrl -i pclsender add_data_stream $PCL_NAME
+switcher-ctrl -S $SENDER_URL -i pclsender add_data_stream $PCL_NAME
 sleep 1
-switcher-ctrl -i pclsender add_destination pclmixer_1 $DESTINATION_IP_1
+switcher-ctrl -S $SENDER_URL -i pclsender add_destination pclmixer_1 $DESTINATION_IP_1
 sleep 1
-switcher-ctrl -i pclsender add_destination pclmixer_2 $DESTINATION_IP_2
+switcher-ctrl -S $SENDER_URL -i pclsender add_destination pclmixer_2 $DESTINATION_IP_2
 sleep 1
-switcher-ctrl -i pclsender add_udp_stream_to_dest $PCL_NAME pclmixer_1 21650
+switcher-ctrl -S $SENDER_URL -i pclsender add_udp_stream_to_dest $PCL_NAME pclmixer_1 21650
 sleep 1
-switcher-ctrl -i pclsender add_udp_stream_to_dest $PCL_NAME pclmixer_2 21652
+switcher-ctrl -S $SENDER_URL -i pclsender add_udp_stream_to_dest $PCL_NAME pclmixer_2 21652
 
 # Send sound
-echo "------ PULSE ------"
-switcher-ctrl -C gstsrc mic
+echo '------  -S $SENDER_URLPULSE ------'
+switcher-ctrl -S $SENDER_URL -C gstsrc mic
 sleep 1
-switcher-ctrl -i mic to_shmdata 'pulsesrc device=alsa_input.usb-PreSonus_Audio_AudioBox_USB-01-USB.analog-stereo volume=1 ! capsfilter caps="audio/x-raw-int, endianness=(int)1234, signed=(boolean)true, width=(int)16, depth=(int)16, rate=(int)44100, channels=(int)1" !  audioresample ! audioconvert'
+switcher-ctrl -S $SENDER_URL -i mic to_shmdata 'pulsesrc device=alsa_input.usb-PreSonus_Audio_AudioBox_USB-01-USB.analog-stereo volume=1 ! capsfilter caps="audio/x-raw-int, endianness=(int)1234, signed=(boolean)true, width=(int)16, depth=(int)16, rate=(int)44100, channels=(int)1" !  audioresample ! audioconvert'
 sleep 1
-switcher-ctrl -C rtpsession pulsesender
+switcher-ctrl -S $SENDER_URL -C rtpsession pulsesender
 sleep 1
-switcher-ctrl -i pulsesender add_data_stream /tmp/switcher_pulsesender_mic_gstsrc
+switcher-ctrl -S $SENDER_URL -i pulsesender add_data_stream /tmp/switcher_pulsesender_mic_gstsrc
 sleep 1
-switcher-ctrl -i pulsesender add_destination pulsemixer_1 $DESTINATION_IP_1
+switcher-ctrl -S $SENDER_URL -i pulsesender add_destination pulsemixer_1 $DESTINATION_IP_1
 sleep 1
-switcher-ctrl -i pulsesender add_destination pulsemixer_2 $DESTINATION_IP_2
+switcher-ctrl -S $SENDER_URL -i pulsesender add_destination pulsemixer_2 $DESTINATION_IP_2
 sleep 1
-switcher-ctrl -i pulsesender add_udp_stream_to_dest /tmp/switcher_pulsesender_mic_gstsrc pulsemixer_1 21654
+switcher-ctrl -S $SENDER_URL -i pulsesender add_udp_stream_to_dest /tmp/switcher_pulsesender_mic_gstsrc pulsemixer_1 21654
 sleep 1
-switcher-ctrl -i pulsesender add_udp_stream_to_dest /tmp/switcher_pulsesender_mic_gstsrc pulsemixer_2 21656
+switcher-ctrl -S $SENDER_URL -i pulsesender add_udp_stream_to_dest /tmp/switcher_pulsesender_mic_gstsrc pulsemixer_2 21656
