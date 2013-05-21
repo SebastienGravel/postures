0a8225c8e574f4a6c4ffad3d74222177d7435cc2
Emmanuel Durand <emmanueldurand@gmail.com>
2013-05-21 11:27:56 -0400

    Teleportation: wip

--- projects/teleportation/scripts/readAudio.sh
+++ projects/teleportation/scripts/readAudio.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 killall gst-launch-0.10
-
-shmaudio /tmp/switcher_default_pulsestream_1_custom_0 &
-shmaudio /tmp/switcher_default_pulsestream_2_custom_0
+gst-launch shmsrc socket-path=/tmp/switcher_default_pulsestream_1_custom_0 ! gdpdepay ! decodebin2 ! pulsesink sync=false &
+gst-launch shmsrc socket-path=/tmp/switcher_default_pulsestream_2_custom_0 ! gdpdepay ! decodebin2 ! pulsesink sync=false
